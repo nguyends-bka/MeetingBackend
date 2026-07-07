aca048734c9ca6b68e6b85ed0b0a912179f563b9
nguyends-bka <nguyends.bka@gmail.com>
2026-07-07 21:12:18 +0700

    update URL FaceMatching

--- Options/FaceMatchingOptions.cs
+++ Options/FaceMatchingOptions.cs
@@ -2,6 +2,6 @@ namespace MeetingBackend.Options;
 
 public class FaceMatchingOptions
 {
-    public string Url { get; set; } = "http://54.169.201.65:8080/match";
+    public string Url { get; set; } = "http://100.68.239.124:8080/match";
     public int TimeoutSeconds { get; set; } = 20;
 }
